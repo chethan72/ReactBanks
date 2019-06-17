4138bbc9085b346eca002f43582af5cba9437f85
chethan72 <51792103+chethan72@users.noreply.github.com>
2019-06-17 16:57:25 +0530

    Updates

---
+++ precache-manifest.887e3f362b4b538622856f47d6de0cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e3228609f8ec770ffe6f649043817",
+    "url": "/ReactBanks/index.html"
+  },
+  {
+    "revision": "8cf4204b03e16498a0a2",
+    "url": "/ReactBanks/static/css/main.9e4b045f.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/ReactBanks/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8cf4204b03e16498a0a2",
+    "url": "/ReactBanks/static/js/main.59bc036a.chunk.js"
+  },
+  {
+    "revision": "95eb13c8bb55c965a14b",
+    "url": "/ReactBanks/static/js/runtime~main.37e1c244.js"
+  }
+]);
